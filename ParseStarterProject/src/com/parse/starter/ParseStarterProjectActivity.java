84c8f8a425a41df9c6bfbc3f20503d2767d73ae7
Daniel Chiu <daniechiu@gmail.com>
2015-02-27 11:18:15 -0800

    Adds location tracking so photos can be geotagged.

--- ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
+++ ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -1,31 +1,77 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.parse.ParseAnalytics;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 
 import java.io.ByteArrayOutputStream;
 
-public class ParseStarterProjectActivity extends Activity {
+public class ParseStarterProjectActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private ImageView mImageView;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationManager mLocationManager;
 
-	/** Called when the activity is first created. */
+    /** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
         mImageView = (ImageView) findViewById(R.id.image_view);
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                mLastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                locationListener);
+        buildGoogleApiClient();
 	}
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -49,12 +95,33 @@ public class ParseStarterProjectActivity extends Activity {
             file.saveInBackground();
 
             ParseObject imgUpload = new ParseObject("ImageUpload");
-            imgUpload.put("ImageFile", file);
+            imgUpload.put("File", file);
+
+            //mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            mLastLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (mLastLocation != null) {
+                imgUpload.put("Latitude", mLastLocation.getLatitude());
+                imgUpload.put("Longitude", mLastLocation.getLongitude());
+            }
             imgUpload.saveInBackground();
         }
     }
 
     public void takePhoto(View view) {
         dispatchTakePictureIntent();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
