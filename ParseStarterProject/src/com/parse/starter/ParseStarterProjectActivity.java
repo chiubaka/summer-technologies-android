02ffa4fd3efd458dd7f6f4e4e8e7237739fab2de
Daniel Chiu <daniechiu@gmail.com>
2015-02-27 10:09:11 -0800

    Updates .gitignore to ignore additional build files. Adds ability to take simple photos.

--- ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
+++ ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -1,20 +1,51 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
 
 public class ParseStarterProjectActivity extends Activity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView mImageView;
+
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
 
 		ParseAnalytics.trackAppOpenedInBackground(getIntent());
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
         testObject.saveInBackground();
 	}
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void takePhoto(View view) {
+        dispatchTakePictureIntent();
+    }
 }
